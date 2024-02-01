93b6d0b704e474f98fab9a6f7a5b8354888cdb27
gongyh <gongyh@qibebt.ac.cn>
2024-02-01 19:29:57 +0800

    minimum python version

--- setup.py
+++ setup.py
@@ -13,12 +13,9 @@ setup(
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
     ],
     packages=find_packages(),
-    python_requires=">=3.8",
+    python_requires=">=3.4",
     install_requires=[
         "gfapy",
         "biopython",
