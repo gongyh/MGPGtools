3d784280b0e26f0898ec961ffea3c68b4cba5061
Zhousq0824 <13854291622@163.com>
2024-02-01 21:28:45 +0800

    annotation

--- core.py
+++ core.py
@@ -1,8 +1,10 @@
 import logging
 import os
+
 # import multiprocessing
 import pandas as pd
 from pathlib import Path
+
 # from functools import partial
 from utils.meta import *
 from utils.processDF import *
@@ -36,20 +38,20 @@ class Core(object):
         self.genomesNum = int(get_info(self.meta, self.name)["genomesNum"])
         self.ref = get_info(self.meta, self.name)["ref"]
 
+    # 提取gff文件中的基因起始和终止位置写入bed文件中，并将每个基因长度存入字典
     def extract_gff(self, bedPath):
         geneTag = {}
         geneLength = {}
-        export_bedFiles = 1
-        with open(self.gff, "r") as f1:
-            line_count = 0
-            file_count = 1
-            new_file = open(os.path.join(bedPath, str(file_count) + ".bed"), "w")
-            lines = f1.read().strip().split("\n")
+        with open(self.gff, "r") as f:
+            new_file = open(os.path.join(bedPath, "genes.bed"), "w")
+            lines = f.read().strip().split("\n")
+            # line_count = 1
             for line in lines:
                 if line[0] == "#":
                     continue
                 row = line.strip().split("\t")
                 if row[2] == "gene":
+                    # line_count += 1
                     gene = row[8].split(";")[0].replace("ID=gene-", "")
                     tag = self.ref.replace(".", "#") + "#" + row[0]
                     k = tag + ":" + row[3] + "-" + row[4]
@@ -57,19 +59,15 @@ class Core(object):
                     l = int(row[4]) - int(row[3])
                     geneLength[gene] = l
                     new_file.write(tag + "\t" + row[3] + "\t" + row[4] + "\n")
-                    line_count += 1
-                    if line_count == 50:
-                        new_file.close()
-                        file_count += 1
-                        line_count = 0
-                        new_file = open(os.path.join(bedPath, str(file_count) + ".bed"), "w")
+                    # if line_count == 20:
+                    #     break
             new_file.close()
-            export_bedFiles = file_count
-        return geneTag, geneLength, export_bedFiles
+        return geneTag, geneLength
 
-    def changeGeneTag(self, geneTag, dir, csv):
-        filepath = os.path.join(dir, csv)
-        newFilePath = os.path.join(dir, csv.replace(".tsv", ".New.tsv"))
+    # 更换tsv文件第一列的名称
+    def changeGeneTag(self, geneTag, dir, tsv):
+        filepath = os.path.join(dir, tsv)
+        newFilePath = os.path.join(dir, tsv.replace(".tsv", ".New.tsv"))
         with open(filepath, "r") as file:
             lines = file.readlines()
         new_lines = []
@@ -83,57 +81,94 @@ class Core(object):
             file.writelines(new_lines)
 
     def staticCoreGene(self):
+        # 检查临时文件存放目录，没有则创建
         check_directory(os.path.join(self.outdir, "tmp"))
+        # 定义核心基因的字典
         coreGene = {"0-15%": [], "15-95%": [], "95-99%": [], "99-100%": [], "100%": []}
+        # 包含所有基因组的列表
+        genomeList = getPanTxt(self.database, self.name)
+        # 利用odgi构建og格式文件，排序，根据bed文件提取子图，显示子图的paths信息，将子图转化为gfa格式文件
         ogFile = os.path.join(self.outdir, "tmp", self.name + ".sorted.og")
-        geneTag, genel, bedFiles = self.extract_gff(os.path.join(self.outdir, "tmp"))
+        geneTag, genel = self.extract_gff(os.path.join(self.outdir, "tmp"))
+        geneList = list(genel.keys())
         ogBuild(self.gfa, ogFile, self.threads)
-        for i in range(1, bedFiles + 1):
-            extractOGFile = os.path.join(self.outdir, "tmp", str(i) + ".og")
-            extractOgSortedFile = os.path.join(self.outdir, "tmp", str(i) + ".sorted.og")
-            bedFile = os.path.join(self.outdir, "tmp", str(i) + ".bed")
-            tsvFile = os.path.join(self.outdir, "tmp", str(i) + ".tsv")
-            newTsvFile = os.path.join(self.outdir, "tmp", str(i) + ".New.tsv")
-            gfaFile = os.path.join(self.outdir, "tmp", str(i) + ".gfa")
-            ogExtractBed(ogFile, extractOGFile, bedFile, self.threads)
-            ogSort(extractOGFile, extractOgSortedFile, self.threads)
-            ogPath(extractOgSortedFile, tsvFile, self.threads)
-            ogView(extractOgSortedFile, gfaFile, self.threads)
-            nodel = nodeLength(gfaFile)
-            self.changeGeneTag(geneTag, os.path.join(self.outdir, "tmp"), str(i) + ".tsv")
-            df = pd.read_csv(newTsvFile, delimiter="\t")
-            df_merged = mergeDF(df)
-            results = processDfmerged(df_merged, coreGene)
-            # pool = multiprocessing.Pool(processes = 16)
-            # partial_processRow = partial(processRow, df_merged)
-            # results = results.update(pool.map(partial_processRow, df_merged.iterrows()))
-            # results = pool.map(partial_processRow, df_merged.itertuples(index=False))
-            # print(results)
-            # pool.close()
-            # pool.join()
-            for g, v in results.items():
-                currentGeneLength = genel[g]
-                uniqueNum = 0
-                for z in v.values():
-                    length = 0
-                    for q in z:
-                        length = length + nodel[q[5:]]
-                    if length / currentGeneLength < 0.2:
-                        uniqueNum = uniqueNum + 1
-                rate = (self.genomesNum - uniqueNum) / self.genomesNum
-                if rate == 1:
-                    coreGene["100%"].append(g)
-                if 0.99 <= rate < 1:
-                    coreGene["99-100%"].append(g)
-                if 0.95 <= rate < 0.99:
-                    coreGene["95-99%"].append(g)
-                if 0.15 <= rate < 0.95:
-                    coreGene["15-95%"].append(g)
-                if 0 <= rate < 0.15:
-                    coreGene["0-15%"].append(g)
-            print(coreGene)
-        for k, v in coreGene.items():
-            with open(os.path.join(self.outdir, k + ".txt"), "w") as f:
-                for i in v:
-                    f.write(i + "\n")
+        extractOGFile = os.path.join(self.outdir, "tmp", "genes.og")
+        extractOgSortedFile = os.path.join(self.outdir, "tmp", "genes.sorted.og")
+        bedFile = os.path.join(self.outdir, "tmp", "genes.bed")
+        tsvFile = os.path.join(self.outdir, "tmp", "genes.tsv")
+        newTsvFile = os.path.join(self.outdir, "tmp", "genes.New.tsv")
+        gfaFile = os.path.join(self.outdir, "tmp", "genes.gfa")
+        ogExtractBed(ogFile, extractOGFile, bedFile, self.threads)
+        ogSort(extractOGFile, extractOgSortedFile, self.threads)
+        ogPath(extractOgSortedFile, tsvFile, self.threads)
+        ogView(extractOgSortedFile, gfaFile, self.threads)
+        # 根据子图的gfa文件(包含所有基因序列的gfa)，提取每个节点的长度
+        nodel = nodeLength(gfaFile)
+        self.changeGeneTag(geneTag, os.path.join(self.outdir, "tmp"), "genes.tsv")
+        df = pd.read_csv(newTsvFile, delimiter="\t")
+        # 子图的矩阵处理，用来判断每个基因在多少个基因组中是存在的，标准是80%序列相同则判定是存在的
+        df_merged = mergeDF(df)
+        # results字典中的键是基因的ID，值是一个字典，这个字典保存每个基因组中该基因与参考不一致的node
+        # 不在results字典键中的基因是与参考100%相同的基因
+        results = processDfmerged(df_merged, coreGene)
+        absenceGene = {}
+        # pool = multiprocessing.Pool(processes = 16)
+        # partial_processRow = partial(processRow, df_merged)
+        # results = results.update(pool.map(partial_processRow, df_merged.iterrows()))
+        # results = pool.map(partial_processRow, df_merged.itertuples(index=False))
+        # print(results)
+        # pool.close()
+        # pool.join()
+        for g, v in results.items():
+            currentGeneLength = genel[g]
+            uniqueNum = 0
+            for i, z in v.items():
+                length = 0
+                for q in z:
+                    length = length + nodel[q[5:]]
+                if length / currentGeneLength > 0.2:
+                    i = i.replace("#", ".")
+                    uniqueNum = uniqueNum + 1
+                    if g in absenceGene:
+                        absenceGene[g].append(i)
+                    else:
+                        absenceGene[g] = [i]
+            rate = (self.genomesNum - uniqueNum) / self.genomesNum
+            if rate == 1:
+                coreGene["100%"].append(g)
+            if 0.99 <= rate < 1:
+                coreGene["99-100%"].append(g)
+            if 0.95 <= rate < 0.99:
+                coreGene["95-99%"].append(g)
+            if 0.15 <= rate < 0.95:
+                coreGene["15-95%"].append(g)
+            if 0 <= rate < 0.15:
+                coreGene["0-15%"].append(g)
+        total_genes = len(genel)
+        gene_99_100 = len(coreGene["100%"]) + len(coreGene["99-100%"])
+        geneMatrix = pd.DataFrame(index=geneList, columns=genomeList)
+        geneMatrix[:] = 1
+        geneMatrix = geneMatrix.astype(int)
+        for a, b in absenceGene.items():
+            for i in b:
+                geneMatrix.at[a, i] = 0
+        geneMatrix.to_csv(
+            os.path.join(self.outdir, "gene_presence_absence.tsv"), sep="\t", index=True
+        )
+        with open(os.path.join(self.outdir, "summary_statistics.txt"), "w") as f:
+            f.write("Core genes(99% <= strains <= 100%): {}\n".format(gene_99_100))
+            f.write(
+                "Soft core genes(95% <= strains < 99%): {}\n".format(
+                    len(coreGene["95-99%"])
+                )
+            )
+            f.write(
+                "Shell genes(15% <= strains < 95%): {}\n".format(
+                    len(coreGene["15-95%"])
+                )
+            )
+            f.write(
+                "Cloud genes(0% <= strains < 15%): {}\n".format(len(coreGene["0-15%"]))
+            )
+            f.write("Total genes: {}".format(total_genes))
         delete_temp_dir(os.path.join(self.outdir, "tmp"))
--- utils/processDF.py
+++ utils/processDF.py
@@ -21,19 +21,22 @@ import pandas as pd
 #             result[row["path.name"]][r["path.name"]] = columns
 #     return result
 
+
 def processDfmerged(df_merged, coreGene):
     result = {}
     for index, row in df_merged.iterrows():
-        if 'GCF' not in row['path.name'] and 'GCA' not in row['path.name']:
+        if "GCF" not in row["path.name"] and "GCA" not in row["path.name"]:
             temp_df = pd.DataFrame(data=row).transpose()
-            other_rows = df_merged[df_merged['path.name'].str.contains('GCA|GCF')==True]
+            other_rows = df_merged[
+                df_merged["path.name"].str.contains("GCA|GCF") == True
+            ]
             new_df = pd.DataFrame(other_rows)
             for column in temp_df.columns[3:]:
                 if row[column] == 0:
                     new_df.drop(column, axis=1, inplace=True)
             all_zeros = new_df.iloc[:, 3:].apply(lambda x: (x == 0).all(), axis=1)
             if all_zeros.all():
-                coreGene['100%'].append(row['path.name'])
+                coreGene["100%"].append(row["path.name"])
                 continue
             result[row["path.name"]] = {}
             for i, r in new_df.iterrows():
@@ -45,17 +48,21 @@ def processDfmerged(df_merged, coreGene):
                 result[row["path.name"]][r["path.name"]] = columns
     return result
 
+
 def mergeDF(df):
     df_merged = (
-        df.groupby(df.iloc[:, 0].str.split("#").str[0])
+        df.groupby(
+            df.iloc[:, 0].str.split("#").str[:2].str.join("#")
+        )
         .apply(merge_rows)
         .reset_index(drop=True)
     )
     return df_merged
 
+
 def merge_rows(row_group):
     merged_row = row_group.iloc[0].copy()
-    merged_row.iloc[0] = row_group.iloc[0].iloc[0].split("#")[0]
+    merged_row.iloc[0] = "#".join(row_group.iloc[0].iloc[0].split("#")[:2])
     for i in range(1, len(row_group.columns)):
         merged_row.iloc[i] = row_group.iloc[:, i].sum()
     return merged_row
